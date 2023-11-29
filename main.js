9f87a852c6f09b01db00a2b2fc598591813b7f4b
TheCyberiousPizzerious <125442124+TheCyberiousPizzerious@users.noreply.github.com>
2023-11-29 20:34:11 +0100

    EXTREME CSS UPGRADE 200%

--- main.js
+++ main.js
@@ -13,12 +13,20 @@ async function handleFileOpen () {
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
+    // Height controls
+    show: true,
+    width: 810,
+    height: 600,
+    minWidth: 800,
+    minHeight: 480,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js') 
     }
   })
   mainWindow.loadFile('index.html');
+
+  console.log(mainWindow)
   
   // I have no clue is this will break the code // It does
   //if (isDev) {
