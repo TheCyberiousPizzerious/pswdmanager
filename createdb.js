599d0711aae15c2cd2c5b1a33fe055d21e01cd00
TheCyberiousPizzerious <125442124+TheCyberiousPizzerious@users.noreply.github.com>
2023-12-01 16:13:58 +0100

    hjemme

--- createdb.js
+++ createdb.js
@@ -1,38 +1,48 @@
 const { ipcRenderer } = require('electron');
 
 const continueBtn = document.getElementById("Continue-button");
-continueBtn.addEventListener('click', main())
+//continueBtn.addEventListener('click', main())
 const goBackBtn = document.getElementById("Go-Back-button");
 const cancelBtn = document.getElementById("Cancel-button")
 
-let newDBNavn = document.getElementById("newdb-navn").value;
 let newDBBeskrivelse = document.getElementById("newdb-beskrivelse").value;
-let newdbPassord = document.getElementById("newdb-password").value;
-let passwordCheck = document.getElementById("newdb-password-check").value;
-let errM = document.getElementById("error-p").value;
+let errM = document.getElementById("error-p");
 
-console.log("Navnet:" + newDBNavn + "og beskrivelsen:" + newDBBeskrivelse)
 
-function pwdCheck() {
-    if (newdbPassord != passwordCheck) {
-        errM.innerHTML("The password do not match")
+
+continueBtn.addEventListener('click', async () => {
+  let newdbPassord = document.getElementById("newdb-password").value;
+  let passwordCheck = document.getElementById("newdb-password-check").value
+  let newDBNavn = document.getElementById("newdb-navn").value;
+ 
+  console.log("Navnet:" + newDBNavn + "og beskrivelsen:" + newDBBeskrivelse)
+    if (newDBNavn == '' || newdbPassord == '' || passwordCheck == '') {
+      errM.innerHTML = "Please fill inn all the required fields";
+      errorAnimation() // Kanskje dette også må await(es)
     } else {
-      jsonStore()
-    }
+        if (newdbPassord != passwordCheck) {
+          errM.innerHTML = "The passwords do not match";
+        } else {
+            localStorage.setItem("newDBNavn", newDBNavn,"newDBBeskrivelse", newDBBeskrivelse,"newDBPassord", newdbPassord)
+          try {
+            const response = await ipcRenderer.invoke('create-database', localStorage.getItem("newDBnavn"));
+            console.log(response);
+          } catch (error) {
+            console.error('Error creating database:', error);
+          }
+        }
+      }
+}) 
+
+function errorAnimation() {
+  let input = document.getElementsByClassName('newdb-req-input');
+  input.classList.add('fade-out-animation');
+
+  setTimeout(() => {
+    input.classList.remove('fade-out-animation');
+  }, 2000);
 }
 
-function jsonStore() {
-    localStorage.setItem(
-        "newDBNavn", newDBNavn,
-        "newDBBeskrivelse", newDBBeskrivelse,
-        "newDBPassord", newdbPassord
-    );
-}
-
-function main() {
-  pwdCheck();
-  createDBFunction()
-}
 
 // Her må ting lagres i localstorage eller lage en tmp fil eller noe
 
@@ -42,24 +52,18 @@ function main() {
 // at jeg bytter til en ny side
 
 // Kanskje dette er den jeg burde bruke
-async function createDBFunction() {
-  try {
-    const response = await ipcRenderer.invoke('create-database', localStorage.getItem("newDBnavn"));
-    console.log(response);
-  } catch (error) {
-    console.error('Error creating database:', error);
-  }
-};
 
 // Provided vet ikke om noe av dette virker
-document.getElementById('createdb-btn').addEventListener('click', async () => {
-  try {
-    // Sending bing bing bing
-    const response = await ipcRenderer.invoke('create-database', 'mydatabase');
 
-    // Ring ding ding recieved
-    console.log(response);
-  } catch (error) {
-    console.error('Error creating database:', error);
-  }
-});
+
+//document.getElementById('createdb-btn').addEventListener('click', async () => {
+//  try {
+//    // Sending bing bing bing
+//    const response = await ipcRenderer.invoke('create-database', 'mydatabase');
+//
+//    // Ring ding ding recieved
+//    console.log(response);
+//  } catch (error) {
+//    console.error('Error creating database:', error);
+//  }
+//});
